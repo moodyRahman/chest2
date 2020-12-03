914be7af8f2d3699a0ca6a700319baa628747185
Moody Rahman <mrahman01@stuy.edu>
2020-12-03 12:17:15 -0500

    wrap app in contexts

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Switcher from "./Switch.js"
-import {UserContext} from "./context/Context.js"
+import {UserContext, TabContext} from "./context/Context.js"
 import { useState } from 'react'
 
 function App() {
   const [data, setData] = useState("helloooo");
+  const [tab, setTab] = useState(0)
   return (
     <Router>
     <UserContext.Provider value={{data, setData}}>
+    <TabContext.Provider value={{tab, setTab}}>
       <Switcher />
+    </TabContext.Provider>
     </UserContext.Provider>
     </Router>
   );
