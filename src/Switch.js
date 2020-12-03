16b52500fcaf28e6e862a0e667d062fbd8344a34
Moody Rahman <mrahman01@stuy.edu>
2020-12-03 09:09:24 -0500

    context provides to all children

--- src/Switch.js
+++ src/Switch.js
@@ -6,19 +6,24 @@ import {
 } from "react-router-dom";
 import Home from "./home/Home.js"
 import Chars from "./chars/Chars.js"
+import Login from "./login/Login.js"
 
 function Switcher() {
 	return (
 		<Switch>
 			<Route path="/chars">
-				{/* TODO: IMPLEMENT */}
-				{/* <Chars /> */}
 				<Chars />
 			</Route>
+
+			<Route path="/login">
+				<Login />
+			</Route>
+			
 			<Route path="/">
-				{/* TODO: IMPLEMENT */}
 				<Home />
 			</Route>
+
+			
 		</Switch>
 	);
 }
