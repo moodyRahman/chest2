43028e682dca8444a409f2a5fc113ade30aa1add
Moody Rahman <mrahman01@stuy.edu>
2020-12-11 22:03:40 -0500

    skeleton code for home

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,19 +2,25 @@
 import Nav from "../Nav.js"
 import Container from '@material-ui/core/Container';
 import { Button } from "@material-ui/core";
+import { UserContext, TokenContext } from '../context/Context.js';
+import React, { useContext, useState } from 'react';
 
 function Home()
 {
 
+	const { token, setToken } = useContext(TokenContext)
+	const { user, setUser } = useContext(UserContext)
+
+	
+
 	return(
 		<Container maxWidth="xl">
 
-			{/* <Container maxWidth="xl">
-				<Nav />
-			</Container> */}
-
 			<Container>
 				HOME
+			</Container>
+			<Container>
+				{token}
 			</Container>
 
 		</Container>
