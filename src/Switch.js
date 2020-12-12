6e3ee314eed67c5315f240cf762eada30137fa11
Moody Rahman <mrahman01@stuy.edu>
2020-12-11 22:04:26 -0500

    switch statement according to loggedin state

--- src/Switch.js
+++ src/Switch.js
@@ -7,22 +7,46 @@ import {
 import Home from "./home/Home.js"
 import Chars from "./chars/Chars.js"
 import Login from "./login/Login.js"
+import Help from "./help/Help.js"
+import {UserContext} from "./context/Context.js"
+import { useContext } from "react";
 
 function Switcher() {
+
+	const { user, setUser } = useContext(UserContext)
+
+	if (!user)
+	{
+		return (
+			<Switch>
+				<Route path="/help" exact={true}>
+					<Help />
+				</Route>
+
+				<Route path="/login" exact={true}>
+					<Login />
+				</Route>
+				
+				<Route path="/" exact={true}>
+					<Login />
+				</Route>
+			</Switch>
+		);
+	}
+
 	return (
 		<Switch>
-			<Route path="/chars">
+			<Route path="/chars" exact={true}>
 				<Chars />
 			</Route>
-
-			<Route path="/login">
-				<Login />
-			</Route>
 			
-			<Route path="/">
+			<Route path="/help" exact={true}>
+				<Help />
+			</Route>
+
+			<Route path="/" exact={true}>
 				<Home />
 			</Route>
-
 			
 		</Switch>
 	);
