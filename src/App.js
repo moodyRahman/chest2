0c9ba974843ca772d96e1ba78c3d093c5b0c549e
Moody Rahman <mrahman01@stuy.edu>
2020-12-11 22:31:11 -0500

    removed unneeded code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
 } from "react-router-dom";
 import './App.css';
 import Switcher from "./Switch.js"
-import { UserContext, TokenContext } from "./context/Context.js"
+import { UserContext, TokenContext, TabContext } from "./context/Context.js"
 import { useState } from 'react'
 import Nav from "./Nav";
 
 function App() {
   const [user, setUser] = useState("");
   const [token, setToken] = useState("");
+  const [tab, setTab] = useState(0);
+
   return (
     <Router>
+    <TabContext.Provider value={{tab, setTab}}>
       <TokenContext.Provider value={{ token, setToken }}>
         <UserContext.Provider value={{ user, setUser }}>
           <Nav />
           <Switcher />
         </UserContext.Provider>
       </TokenContext.Provider>
+    </TabContext.Provider>
     </Router>
   );
 }
