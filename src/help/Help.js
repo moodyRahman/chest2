be10c15449f062dab39f196cfda938f236aea78c
Moody Rahman <mrahman01@stuy.edu>
2020-12-11 22:03:47 -0500

    basic container for help

---
+++ src/help/Help.js
@@ -0,0 +1,24 @@
+
+
+import Nav from "../Nav.js"
+import Container from '@material-ui/core/Container';
+import { Button } from "@material-ui/core";
+import { UserContext, TokenContext } from '../context/Context.js';
+import React, { useContext, useState } from 'react';
+
+function Help() {
+
+
+	return (
+		<Container maxWidth="xl">
+
+			<Container>
+				HELP
+			</Container>
+
+		</Container>
+
+	);
+}
+
+export default Help;
