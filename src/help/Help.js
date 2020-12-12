f2349ee8761e93f89ce037e969109c08a0230596
Moody Rahman <mrahman01@stuy.edu>
2020-12-11 22:31:55 -0500

    removed unneeded dependencies

--- src/help/Help.js
+++ src/help/Help.js
@@ -1,11 +1,6 @@
 
 
-import Nav from "../Nav.js"
 import Container from '@material-ui/core/Container';
-import { Button } from "@material-ui/core";
-import { UserContext, TokenContext } from '../context/Context.js';
-import React, { useContext, useState } from 'react';
-
 function Help() {
 
 
