6d1f102e9c50e7654811026f402572813eca8a6e
Moody Rahman <mrahman01@stuy.edu>
2020-12-12 19:58:03 -0500

    register page

---
+++ src/register/Register.js
@@ -0,0 +1,21 @@
+import Container from '@material-ui/core/Container';
+
+
+
+
+function Register() {
+
+
+	return (
+		<Container maxWidth="xl">
+
+			<Container>
+				REGISTER
+			</Container>
+
+		</Container>
+
+	);
+}
+
+export default Register;
