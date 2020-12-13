693e35ec33f6be7e139072f22b5ce39680525edb
Moody Rahman <mrahman01@stuy.edu>
2020-12-12 20:19:01 -0500

    register logic

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,9 +1,54 @@
 import Container from '@material-ui/core/Container';
+import { UserContext, TokenContext, TabContext } from '../context/Context.js';
+import { useContext, useState } from 'react';
+import { Button } from '@material-ui/core';
 
 
 
 
 function Register() {
+
+	const [username, setUsername] = useState("");
+	const [password0, setPassword0] = useState("");
+	const [password1, setPassword1] = useState("");
+	const [message, setMessage] = useState("");
+
+	const { user, setUser } = useContext(UserContext)
+	const { token, setToken } = useContext(TokenContext)
+	const { tab, setTab } = useContext(TabContext);
+
+
+	const registerVerify = (event) => {
+		event.preventDefault();
+		
+
+
+	}
+
+	const register = (event) => {
+		event.preventDefault();
+		const data = {
+			"username": username,
+			"password": password0
+		}
+
+		fetch("http://localhost:5000/register", {
+			method: 'POST', // *GET, POST, PUT, DELETE, etc
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data) // body data type must match "Content-Type" header
+		})
+			.then(res => res.json())
+			.then(response => {
+				console.log(response)
+				if (response.status === 200) {
+					setToken(response.token)
+					setUser(response.user)
+					setTab(0)
+				}
+			})
+	}
 
 
 	return (
